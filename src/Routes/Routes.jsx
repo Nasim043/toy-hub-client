45e18701fbe01163415819e7ddf25422cc94b0f2
Saleh Nasim <nasimcse16@gmail.com>
2023-05-17 23:26:41 +0600

    setup basic routes and footer

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Login from "../Pages/Auth/Login";
+import Register from "../Pages/Auth/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Register></Register>
+      }
+    ]
+  },
+]);
+
+export default router;
