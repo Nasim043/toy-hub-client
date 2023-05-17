ed0f3d9620f0f871dc9c2051ebf9c012ba9f4d4c
Saleh Nasim <nasimcse16@gmail.com>
2023-05-18 00:59:36 +0600

    setup auth context provider

--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 
 const Navbar = () => {
+  const { loading } = useContext(AuthContext)
+  console.log(loading);
   return (
     <div>
       <h1>navbar</h1>
